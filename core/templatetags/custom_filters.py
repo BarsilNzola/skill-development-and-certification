4762b6fb6d3c0b705a5bef262a9a50c9ad92a066
BarsilNzola <barsiljohn@gmail.com>
2024-12-10 11:34:31 +0300

    styled lesson content

---
+++ core/templatetags/custom_filters.py
@@ -0,0 +1,31 @@
+import re
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django import template
+
+register = template.Library()
+
+@register.filter
+def render_codeblocks(value):
+    """
+    Replace Markdown-style code blocks (triple backticks) with HTML <pre><code> blocks.
+    Escapes HTML tags inside the code block and converts newlines to <br> for line breaks.
+    """
+    def replace_codeblock(match):
+        code_content = match.group(1)  # Get the content inside the triple backticks
+        # Escape HTML tags to show them as text
+        escaped_content = escape(code_content)
+        # Replace newlines with <br> for rendering line breaks
+        formatted_content = escaped_content.replace("\n", "<br>")
+        return f'<pre><code>{formatted_content}</code></pre>'
+
+    # Use regex to find and replace triple backticks with <pre><code> blocks
+    formatted_value = re.sub(
+        r'```(.*?)```',  # Match content between triple backticks
+        replace_codeblock,  # Apply the escaping and formatting function
+        value,
+        flags=re.DOTALL  # Match across multiple lines
+    )
+
+    # Mark the final string as safe to render HTML in the template
+    return mark_safe(formatted_value)
