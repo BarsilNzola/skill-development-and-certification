f2fe3a60098efc7b8539e67b297a8cd52726f91a
BarsilNzola <barsiljohn@gmail.com>
2024-12-12 16:15:25 +0300

    Update database settings for Render deployment

--- skill_dev_platform/settings.py
+++ skill_dev_platform/settings.py
@@ -84,16 +84,25 @@ WSGI_APPLICATION = 'skill_dev_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'skilldevelopment_db',
-        'USER': 'root',
-        'PASSWORD': 'Anderson25$',
-        'HOST': 'localhost',
-        'PORT': '3306',
+if os.getenv('RENDER'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
     }
-}
+else:
+    # Local database settings
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
