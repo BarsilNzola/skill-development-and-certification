81a627645e4c398a901b0af1fa3148cf9eaf44a5
BarsilNzola <barsiljohn@gmail.com>
2025-05-28 11:21:00 +0300

    changes

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,25 @@
 from rest_framework import serializers
 from .models import Course, Module, Lesson, Progress, Question, Quiz, Assignment
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'confirm_password')
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(**validated_data)
+        return user
+
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
